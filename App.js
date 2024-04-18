a76d50a2fbd90f0ad9c51a9503d41dccad2a9948
LuisAbdielRivera <147383444+LuisAbdielRivera@users.noreply.github.com>
2024-04-18 16:15:03 -0600

    update

--- App.js
+++ App.js
@@ -1,23 +1,19 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import * as React from 'react';
-import HomeScreen from './screens/HomeScreen';
+import { View, Text } from 'react-native'
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import HomeScreen from './screens/HomeScreen.js'
+
+const Stack = createNativeStackNavigator()
 
 const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen 
-        name="HomeScreen" 
-        component={HomeScreen} 
-        options={({navigation})=>({
-          title:'App',
-          headerTitleStyle:{ fontWeight: 'bold'}
-        })}
-        />
+        <Stack.Screen name='App' component={HomeScreen}/>
       </Stack.Navigator>
     </NavigationContainer>
-  );
+  )
 }
 
-export default App;
+export default App
